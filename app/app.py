0ed0adc9b2f9a007b20e7ce1af74a9c502c72897
Gustavo Silva <gustavo.silva.nav@gmail.com>
2025-08-22 21:16:09 -0300

    🚧 Getting my first attemp of cli

--- app/app.py
+++ app/app.py
@@ -1,11 +1,15 @@
+import typer
 import asyncio
-from typing import Any
+from typing import Any, List
 import httpx
+from typing_extensions import Annotated
 import os
 from dotenv import load_dotenv
 
+# Load environment variables
 load_dotenv()
 
+# Environment variables and headers configuration
 NOTION_API_TOKEN = os.getenv("NOTION_API_KEY", "super_secret")
 NOTION_PARENT_PAGE_ID = os.getenv("NOTION_PARENT_PAGE_ID", "super_secret_id")
 NOTION_URI = os.getenv("NOTION_URL", "https://api.notion.com/v1")
@@ -17,7 +21,11 @@ HEADERS = {
     "Notion-Version": NOTION_VERSION,
 }
 
+# Initialize the Typer application
+app = typer.Typer(help="An AI assistant to put your ideas into notion.")
 
+
+# Async function to create pages in Notion
 async def create_new_pages_in_notion(payload: dict[str, Any]):
     async with httpx.AsyncClient() as client:
         resp = await client.post(
@@ -27,7 +35,8 @@ async def create_new_pages_in_notion(payload: dict[str, Any]):
         return resp.json()
 
 
-async def create_new_page(title: str, paragraphs: list[str]):
+# Async function to build the page and send the request
+async def create_notion_page(title: str, paragraphs: List[str]):
     paragraphs_children = [
         {
             "object": "block",
@@ -44,10 +53,42 @@ async def create_new_page(title: str, paragraphs: list[str]):
         "children": paragraphs_children,
     }
 
-    page = await create_new_pages_in_notion(payload=payload)
-    print(page)
+    try:
+        page = await create_new_pages_in_notion(payload=payload)
+        typer.echo(f"Successfully created a new Notion page with ID: {page['id']}")
+    except httpx.HTTPStatusError as err:
+        typer.echo(f"Error creating Notion page: {err.response.text}", err=True)
+
+
+@app.command(name="new-page")
+def cli_add_new_page(
+    title: Annotated[
+        str,
+        typer.Option(
+            ...,
+            prompt="Enter page title",
+            help="The title of the new Notion page.",
+        ),
+    ],
+    paragraphs: Annotated[
+        List[str],
+        typer.Option(
+            ...,
+            "--paragraph",
+            help="Paragraphs for the page content.",
+        ),
+    ],
+):
+    """
+    Creates a new page in Notion.
+    """
+    asyncio.run(create_notion_page(title=title, paragraphs=paragraphs))
+
+
+@app.command("create")
+def cli_create_user(username: str):
+    print(f"Creating user: {username}")
 
 
 if __name__ == "__main__":
-    paragraphs = ["First paragraph", "Second paragraph"]
-    asyncio.run(create_new_page(title="Test page", paragraphs=paragraphs))
+    app()
